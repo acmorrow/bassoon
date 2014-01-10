5c15c3a33c69dd82f9566323fcac8b0a2df1c3e6
Andrew Morrow <acm@10gen.com>
2014-01-10 17:21:05 -0500

    Fixup dump_bytes_as_hex

--- src/lib/bassoon/debug.cpp
+++ src/lib/bassoon/debug.cpp
@@ -1,20 +1,26 @@
 #include <bassoon/debug.hpp>
 
+#include <ostream>
+
+#include <bassoon/bson.hpp>
 
 namespace bassoon {
+  namespace bson {
 
-  /// Dumps 'length' number of bytes from 'data' to the stream 'stream'. The format
-  /// is \\xXX where XX is a hex code.
-  void dump_bytes_as_hex(std::ostream& stream, void const* data, std::size_t length) {
-    static const char items[] = { '0', '1', '2', '3',
-                                  '4', '5', '6', '7',
-                                  '8', '9', 'a', 'b',
-                                  'c', 'd', 'e', 'f' };
+    void dump_bytes_as_hex(
+        std::ostream& stream,
+        void const* data, std::size_t length) {
 
-    byte_t const* cdata = reinterpret_cast<byte_t const*>(data);
-    for (std::size_t i = 0; i != length; ++i) {
-      stream << "\\x" << items[(cdata[i] & 0xf0) >> 4] << items[(cdata[i] & 0x0f)];
+      static const char items[] = { '0', '1', '2', '3',
+                                    '4', '5', '6', '7',
+                                    '8', '9', 'a', 'b',
+                                    'c', 'd', 'e', 'f' };
+
+      byte_t const* cdata = reinterpret_cast<byte_t const*>(data);
+      for (std::size_t i = 0; i != length; ++i) {
+        stream << "\\x" << items[(cdata[i] & 0xf0) >> 4] << items[(cdata[i] & 0x0f)];
+      }
     }
-  }
 
+  } // namespace bson
 } // namespace bassoon
--- src/lib/bassoon/debug.hpp
+++ src/lib/bassoon/debug.hpp
@@ -1,14 +1,23 @@
 #ifndef included_17daf617_6d77_4138_9909_4a6bc4522369
 #define included_17daf617_6d77_4138_9909_4a6bc4522369
 
+#include <cstdlib>
 #include <iosfwd>
 
+#include <bassoon/export.h>
+
 namespace bassoon {
+  namespace bson {
 
-  /// Dumps 'length' number of bytes from 'data' to the stream 'stream'. The format
-  /// is \\xXX where XX is a hex code.
-  void dump_bytes_as_hex(std::ostream& stream, void const* data, std::size_t length);
+    ///
+    /// Dumps 'length' number of bytes from 'data' to the stream 'stream'. The format
+    /// is \\xXX where XX is a hex code.
+    ///
+    void LIBBASSOON_EXPORT dump_bytes_as_hex(
+        std::ostream& stream,
+        void const* data, std::size_t length);
 
+  } // namespace bson
 } // namespace bassoon
 
 #endif // included_17daf617_6d77_4138_9909_4a6bc4522369
