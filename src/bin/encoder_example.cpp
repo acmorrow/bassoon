7f1c047d8f0f1c53d4775d9533e7b86936766026
Andrew Morrow <acm@10gen.com>
2014-01-11 16:58:49 -0500

    Move poorly named file and integrate and udpate

--- src/bin/encoder_example.cpp
+++ src/bin/encoder_example.cpp
@@ -5,30 +5,25 @@
 #include <iostream>
 #include <iomanip>
 
+#include <bassoon/array_writer.hpp>
+#include <bassoon/debug.hpp>
 #include <bassoon/encoder.hpp>
-#include <bassoon/decoder.hpp>
 
 namespace {
 
   using namespace bassoon::bson;
 
-  void dump_bytes(std::ostream& stream, void const* data, std::size_t length) __attribute__((noinline));
-  void dump_bytes(std::ostream& stream, void const* data, std::size_t length) {
-    std::cout << std::string(reinterpret_cast<char const*>(data), length);
-  }
-
-
   bool do_example(std::ostream& stream) __attribute__((noinline));
   bool do_example(std::ostream& stream) {
     // { "hello" : "world" }
     std::array<char, 128> buffer;
-    auto writer = make_writer(buffer);
+    auto writer = make_array_writer(buffer);
     auto document = start_document(writer);
     document.encode_utf8_string("hello", "world");
     document.finish();
 
-    if (document.ok())
-      dump_bytes(stream, buffer.begin(), writer.valid());
+    dump_bytes_as_hex(stream, buffer.begin(), writer.valid());
+    std::cout << "\n";
 
     return document.ok();
   }
@@ -37,15 +32,15 @@ namespace {
   bool do_nested_example(std::ostream& stream) {
     // { "message" : { "hello" : "world" } }
     std::array<char, 128> buffer;
-    auto writer = make_writer(buffer);
+    auto writer = make_array_writer(buffer);
     auto document = start_document(writer);
     auto message = document.start_subdocument("message");
     message.encode_utf8_string("hello", "world");
     message.finish();
     document.finish();
 
-    if (document.ok())
-      dump_bytes(stream, buffer.begin(), writer.valid());
+    dump_bytes_as_hex(stream, buffer.begin(), writer.valid());
+    std::cout << "\n";
 
     return document.ok();
   }
@@ -54,15 +49,15 @@ namespace {
   bool do_nested_abstract(std::ostream& stream) {
     // { "message" : { "hello" : "world" } }
     std::array<char, 128> buffer;
-    auto writer = make_writer(buffer);
+    auto writer = make_array_writer(buffer);
     auto document = start_document(writer);
     auto message = document.abstract_start_subdocument("message");
     message->encode_utf8_string("hello", "world");
     message->finish();
     document.finish();
 
-    if (document.ok())
-      dump_bytes(stream, buffer.begin(), writer.valid());
+    dump_bytes_as_hex(stream, buffer.begin(), writer.valid());
+    std::cout << "\n";
 
     return document.ok();
   }
@@ -71,7 +66,7 @@ namespace {
   bool do_array(std::ostream& stream) {
     // { "array" : [ "hello", "world" ] }
     std::array<char, 128> buffer;
-    auto writer = make_writer(buffer);
+    auto writer = make_array_writer(buffer);
     auto document = start_document(writer);
     auto array = document.start_subarray("array");
     array.encode_utf8_string("0", "hello");
@@ -79,15 +74,10 @@ namespace {
     array.finish();
     document.finish();
 
-    if (document.ok())
-      dump_bytes(stream, buffer.begin(), writer.valid());
+    dump_bytes_as_hex(stream, buffer.begin(), writer.valid());
+    std::cout << "\n";
 
     return document.ok();
-  }
-
-  void do_decode() {
-
-
   }
 
 } // namespace
@@ -97,4 +87,5 @@ int main(int argc, char* argv[]) {
   do_nested_example(std::cout);
   do_nested_abstract(std::cout);
   do_array(std::cout);
+  return EXIT_SUCCESS;
 }
